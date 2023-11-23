aa3c7a3965b171c093fec095d2a44b9093c86eda
Bairen0 <lovesmath314@gmail.com>
2023-11-23 10:11:24 -0600

    Move ws creation to handler

--- main.py
+++ main.py
@@ -1,22 +1,14 @@
 import logging
 import logging.config
-import websocket
 import rel
-import Config
-import IrcMessageHandler
+from IrcMessageHandler import messageHandlerInit
 
 logging.config.fileConfig('logging.conf')
 logger = logging.getLogger("shepbot")
 
 if __name__ == "__main__":
     #websocket.enableTrace(True)
-    IrcMessageHandler.messageHandlerInit()
-    ws = websocket.WebSocketApp("wss://irc-ws.chat.twitch.tv",
-                              on_open=IrcMessageHandler.on_open,
-                              on_message=IrcMessageHandler.on_message,
-                              on_error=IrcMessageHandler.on_error,
-                              on_close=IrcMessageHandler.on_close)
+    messageHandlerInit()
 
-    ws.run_forever(dispatcher=rel, reconnect=5)  # Set dispatcher to automatic reconnection, 5 second reconnect delay if connection closed unexpectedly
     rel.signal(2, rel.abort)  # Keyboard Interrupt
     rel.dispatch()
